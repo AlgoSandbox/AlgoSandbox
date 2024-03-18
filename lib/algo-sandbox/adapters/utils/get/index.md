aa50c469e7a225ee889f5608df616a08e1a492e1
Jeff Sieu <jeffsy00@gmail.com>
2024-03-18 22:37:23 +0800

    feat: implement flexible get

--- lib/algo-sandbox/adapters/utils/get/index.md
+++ lib/algo-sandbox/adapters/utils/get/index.md
@@ -1,3 +1,12 @@
-# get
+# Get at path
 
 Gets the value at `path` of `object`.
+
+This supports `*` syntax for arrays/objects.
+
+Example:
+
+```ts
+// e.g. customGet({ a: { b: [1, 2, 3] } }, 'a.b') => [1, 2, 3]
+// e.g. customGet([{b: 2}, {b: 3}], '*.b') => [2, 3]
+```
--- lib/algo-sandbox/adapters/utils/get/index.ts
+++ lib/algo-sandbox/adapters/utils/get/index.ts
@@ -1,9 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import {
   createParameterizedAdapter,
   SandboxParam,
   SandboxStateType,
 } from '@algo-sandbox/core';
-import { get } from 'lodash';
 import { z } from 'zod';
 
 const inputState = {
@@ -28,8 +28,37 @@ const getAdapter = createParameterizedAdapter({
   accepts: () => inputState,
   outputs: () => outputState,
   transform: (input, parameters) => {
+    function get(object: any, path: string): any {
+      const paths = path.split('.');
+      let result = object;
+      for (const p of paths) {
+        result = result[p];
+      }
+      return result;
+    }
+    // custom get which supports * syntax for arrays/objects
+    // e.g. customGet({ a: { b: [1, 2, 3] } }, 'a.b') => [1, 2, 3]
+    // e.g. customGet([{b: 2}, {b: 3}], '*.b') => [2, 3]
+    function customGet(object: any, path: string): any {
+      // try to defer to lodash get if possible
+      if (path.includes('*')) {
+        const parts = path.split('.');
+        const [firstPart, ...rest] = parts;
+        if (firstPart === '*') {
+          if (Array.isArray(object)) {
+            return object.map((o: any) => customGet(o, rest.join('.')));
+          } else {
+            return Object.values(object).map((o: any) =>
+              customGet(o, rest.join('.')),
+            );
+          }
+        }
+      }
+      return get(object, path);
+    }
+
     return {
-      valueAtPath: get(input.object, parameters.path),
+      valueAtPath: customGet(input.object, parameters.path),
     };
   },
 });
